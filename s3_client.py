edea4e7f745f5a44b2eb1dd8a014b939cf864777
ARUN NIVAAS AR C <168203480+arun-nivaas@users.noreply.github.com>
2025-09-14 20:25:02 +0530

    aws config small changes updated.

--- s3_client.py
+++ s3_client.py
@@ -7,7 +7,7 @@ import boto3
 load_dotenv(dotenv_path=".env", override=True)
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')        
-AWS_REGION= os.getenv('AWS_REGION', 'AWS_REGION')
+AWS_REGION= os.getenv('AWS_REGION', 'ap-south-1')
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
 s3_client = boto3.client(
